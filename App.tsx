dc452a6f14971eb6a4c0d2db64fda07a92d689fe
MarcosMarossi <marcos.marossi@fatec.gov.sp.br>
2023-10-14 13:03:18 -0300

    Ajustes de configuração

--- App.tsx
+++ App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 import Routes from './src/routes';
 import { FarmerMarketProvider } from './src/store';
-import { StatusBar } from 'react-native';
 
 export default function App() {
   return (
     <>
       <FarmerMarketProvider>
         <PaperProvider>
-          <StatusBar barStyle={"dark-content"} backgroundColor="transparent" translucent />
           <Routes />
         </PaperProvider>
       </FarmerMarketProvider>
